fc3667a8fba314ebf6037822f3abc28fcc8167a8
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2021-12-12 20:13:44 +0800

    Added the input test code.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Tutorial: Tour of Heros : https://angular.io/tutorial/toh-pt0
 
  
+
+```
+ng new angular-tour-of-heroes
+```
+
+`ng generate component heroes`
+
+**[(ngModel)]** is Angular's two-way data binding syntax.
--- src/angular-tour-of-heroes/src/app/app.module.ts
+++ src/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
