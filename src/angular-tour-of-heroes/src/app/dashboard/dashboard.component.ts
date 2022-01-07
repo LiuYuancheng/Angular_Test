cc6cf25a5efa09a9bb95dc423c7d6a8db1921cd1
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2022-01-07 08:09:13 +0800

    Added the simulation data.

--- src/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ src/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../HeroIntf';
 import { HeroService } from '../hero.service';
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-dashboard',
@@ -10,13 +11,17 @@ import { HeroService } from '../hero.service';
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
+  date = new Date();
+  dateString = '2022/01/06';
+  //public datepipe: DatePipe;
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes()
+    //this.dateString =this.datepipe.transform(this.date, 'yyyy-MM-dd');
   }
 
   getHeroes():void{
-    this.heroService.getHeroes().subscribe(heroes=> this.heroes =heroes.slice(1,5));
+    this.heroService.getHeroes().subscribe(heroes=> this.heroes =heroes.slice(0,4));
   }
 }
--- src/angular-tour-of-heroes/src/app/heroes/data/mock-heroes.ts
+++ src/angular-tour-of-heroes/src/app/heroes/data/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from '../../HeroIntf';
 
 export const HEROS: Hero[] = [
-    { id: 11, name: 'Benign Filter' },
+    { id: 11, name: 'Phishing Pedia' },
     { id: 12, name: 'Newly Registered Domains' },
     { id: 13, name: 'Heuristic Filter' },
     { id: 14, name: 'Lexical Model - Spark ML' },
     { id: 15, name: 'Phishing Model' },
     { id: 16, name: 'NLP - Deep Learning' },
     { id: 17, name: 'CV - Deep Learning' },
-    { id: 18, name: 'Phishing Pedia' },
+    { id: 18, name: 'Benign Filter' },
     { id: 19, name: 'Facebook detectron2 AI' },
     { id: 20, name: 'zvelo AI' }
 ]
